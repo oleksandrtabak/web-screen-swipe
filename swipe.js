041eb4a60c7dc69a21b1b73a627cd3f392471a55
Олександр <oleksandrtabak@protonmail.com>
2022-06-10 16:07:52 +0300

    Відстеження подій натиску і дотику

--- swipe.js
+++ swipe.js
@@ -3,7 +3,7 @@ const down = "🡣"
 const right = "🡢"
 const left = "🡠"
 
-const screen = document.querySelector('.container')
+const slider = document.querySelector('.container')
 const arrow = document.querySelector('.arrow')
 
 let isDragging = false
@@ -12,5 +12,13 @@ let previousPositionX = 0
 let currentPositionY = 0
 let previousPositionY = 0
 
+// Події доторкання до сенсору
+slider.addEventListener('touchstart', () => console.log('Доторкнулися до екрану')) 
+slider.addEventListener('touchend', () => console.log('Дотик закінчено'))
+slider.addEventListener('touchmove', () => console.log('Переміщення по екрану'))
 
-
+// Події натиску мишкою
+slider.addEventListener("mousedown", () => console.log('Мишку натиснуто'))
+slider.addEventListener("mouseup", () => console.log('Мишку відпущено'))
+slider.addEventListener("mouseleave", () => console.log('Мишку виведено за межі'))
+slider.addEventListener("mousemove", () => console.log('Переміщення курсору'))
